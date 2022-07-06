e810b0332b0d4ed5fee00d69009139b68ac54fb8
webia1 <webia1.github@gmail.com>
2022-07-06 21:32:18 +0200

    JS Generators Basic Examples

--- JavaScript/About/Asynchronicity/Examples/Generators-Basic-Examples.ts
+++ JavaScript/About/Asynchronicity/Examples/Generators-Basic-Examples.ts
@@ -1,17 +1,49 @@
 import assert from 'assert/strict';
 
-function* gen() {
-  yield 'a';
-  yield 'b';
+namespace example1 {
+  function* gen() {
+    yield 'a';
+    yield 'b';
+  }
+
+  const iterator = gen();
+  assert.deepEqual(Array.from(iterator), ['a', 'b']);
+
+  console.log(iterator.next(), iterator.next(), iterator.next());
+
+  /**
+   * {value: 'a', done: false}
+   * {value: 'b', done: false}
+   * {value: undefined, done: true}
+   */
 }
 
-const iterator = gen();
-assert.deepEqual(Array.from(iterator), ['a', 'b']);
+namespace example2 {
+  function* genLines() {
+    yield 'Sentence one';
+    yield 'Sentence two';
+  }
 
-console.log(iterator.next(), iterator.next(), iterator.next());
+  function* genNumberLines(genLinesIterable: Generator) {
+    let lineNo = 1;
+    for (const line of genLinesIterable) {
+      yield lineNo++ + ') ' + line;
+    }
+  }
 
-/**
- * {value: 'a', done: false}
- * {value: 'b', done: false}
- * {value: undefined, done: true}
- */
+  const numberedLines = genNumberLines(genLines());
+
+  console.log(
+    numberedLines.next(),
+    numberedLines.next(),
+    numberedLines.next(),
+    numberedLines.next(),
+  );
+
+  /**
+   * { value: '1) Sentence one', done: false }
+   * { value: '2) Sentence two', done: false }
+   * { value: undefined, done: true }
+   * { value: undefined, done: true }
+   */
+}
