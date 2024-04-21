2f82b30a4f1be3a8b0270179db663d69be37b68e
webia1 <webia1.github@gmail.com>
2024-04-21 15:43:34 +0200

    SCSS Mixins

--- GitHooks/Husky/Checks/index.ts
+++ GitHooks/Husky/Checks/index.ts
@@ -2,24 +2,37 @@ const fsHusky = require('fs');
 const exec = require('child_process').exec;
 
 const currentProjectHooks = ['pre-commit', 'pre-push'];
-
 const huskyExecutablesFolder = './.husky/_';
 
 function checkIfHuskyExistsAndNotEmpty() {
   return new Promise(function (resolve) {
     fsHusky.readdir(huskyExecutablesFolder, function (err, files) {
       if (err) {
-        console.log('Husky Folder not found');
-        resolve(false);
+        console.log('Trying to activate Husky...');
+        tryToActivateHusky(resolve);
       } else if (files.length === 0) {
-        console.log('Husky Folder is empty');
-        resolve(false);
+        console.log('Trying to activate Husky...');
+        tryToActivateHusky(resolve);
       } else {
         resolve(true);
       }
     });
   });
 }
+
+// function createHuskyFolder(resolve: (value: unknown) => void) {
+//   fsHusky.mkdir('./.husky/_', function (err) {
+//     if (err) {
+//       console.error(
+//         formatError(`Error creating Husky Folder: ${err.message}`),
+//       );
+//       resolve(false);
+//     } else {
+//       console.log('Husky Folder has been successfully created.');
+//       resolve(false);
+//     }
+//   });
+// }
 
 function checkCustomHooks(hook) {
   return new Promise(function (resolve) {
@@ -28,7 +41,9 @@ function checkCustomHooks(hook) {
       fsHusky.constants.F_OK,
       function (err) {
         if (err) {
-          console.log('Husky Hook not found:', hook);
+          console.error(
+            formatError(`Custom Hook '${hook}' not found!`),
+          );
           resolve(false);
         } else {
           resolve(true);
@@ -38,29 +53,44 @@ function checkCustomHooks(hook) {
   });
 }
 
-function activateHusky() {
+function tryToActivateHusky(resolve: (value: unknown) => void) {
   exec('npx husky', function (error) {
     if (error) {
-      console.error('Error Activating Husky:', error);
-      console.warn('You may need to execute `npm i`.');
+      console.error(
+        formatError(`Error Activating Husky: ${error.message}`),
+      );
+      console.warn('You may need to execute `npm i && npx husky`.');
+      resolve(false);
     } else {
-      console.log('Husky Hooks are present.');
+      console.log('Husky Hooks has been succesfully activated.');
+      resolve(true);
     }
   });
 }
 
+function formatError(message) {
+  const red = '\x1b[31m'; // ANSI Code für Rot
+  const reset = '\x1b[0m'; // Reset Farbe
+  return `${red}${message}${reset}`;
+}
+
 Promise.all([
-  ...currentProjectHooks.map(checkCustomHooks),
   checkIfHuskyExistsAndNotEmpty(),
+  ...currentProjectHooks.map(checkCustomHooks),
 ]).then(function (results) {
   if (results.includes(false)) {
-    console.log(
-      'That should never happen: Custom Husky Hooks are missing or Husky folder is not set up correctly!',
+    console.error(
+      formatError('---------------------------------------------'),
+      formatError('\nERROR: Husky Hooks are not correctly set up!'),
+      formatError('\nPLEASE CONTACT THE ARCHITECTURE TEAM!'),
+      formatError('\nDO NOT COMMIT OR PUSH!'),
+      formatError(
+        '\n---------------------------------------------',
+      ),
     );
-    console.log('Please inform the architecture team.');
   } else {
     console.log(
-      'All checks passed: Custom Husky Hooks and Husky folder are correctly set up.',
+      '✔️  Custom Husky Hooks and Husky folder seem to be correctly set up.',
     );
   }
 });
